03bec5cf37853d4658f275b90adb7c9f0f343fe5
Powerbambus <meister.luke2005@gmail.com>
2026-06-16 22:31:20 +0200

    Added more stations and fixed exact match problem

--- src/algorithms/matcher.cpp
+++ src/algorithms/matcher.cpp
@@ -13,6 +13,11 @@ std::unordered_map<std::string, Station> matcher(std::string input, std::unorder
         transform(station_name.begin(), station_name.end(), station_name.begin() ,::tolower);
         std::string station_id = station_obj.id;
         transform(station_id.begin(), station_id.end(), station_id.begin() ,::tolower);
+        if (station_id == input || station_name == input) {
+            std::unordered_map<std::string, Station> exact_output;
+            exact_output.insert(std::make_pair(station_obj.id, station_obj));
+            return exact_output;
+        }
         if (station_id.find(input) != std::string::npos || station_name.find(input) != std::string::npos) {
             output.insert(std::make_pair(station_obj.id, station_obj));
         }
